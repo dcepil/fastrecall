eeca3f17838ffe547e203ab4481ce302490371f8
d.cepil <db.cepil@gmail.com>
2023-06-06 16:20:26 +0200

    (feat)update to tML 1.4.4

--- Config.cs
+++ Config.cs
@@ -9,20 +9,14 @@ public class Config : ModConfig
     public override ConfigScope Mode => ConfigScope.ServerSide;
 
     [Header("$Mods.FastRecall.Config.Header")]
-    [Label("$Mods.FastRecall.Config.EnabledForMirrors.Label")]
-    [Tooltip("$Mods.FastRecall.Config.EnabledForMirrors.Tooltip")]
     [DefaultValue(true)]
     [ReloadRequired]
     public bool EnabledForMirrors;
 
-    [Label("$Mods.FastRecall.Config.EnabledForCellphone.Label")]
-    [Tooltip("$Mods.FastRecall.Config.EnabledForCellphone.Tooltip")]
     [DefaultValue(true)]
     [ReloadRequired]
     public bool EnabledForCellphone;
 
-    [Label("$Mods.FastRecall.Config.SpeedFactor.Label")]
-    [Tooltip("$Mods.FastRecall.Config.SpeedFactor.Tooltip")]
     [Range(1, 10)]
     [Increment(1)]
     [Slider]
@@ -31,8 +25,6 @@ public class Config : ModConfig
     [ReloadRequired]
     public int SpeedFactor;
 
-    [Label("$Mods.FastRecall.Config.MirrorLikeItems.Label")]
-    [Tooltip("$Mods.FastRecall.Config.MirrorLikeItems.Tooltip")]
     [ReloadRequired]
     public List<ItemDefinition> MirrorLikeItems = new List<ItemDefinition>();
 }
